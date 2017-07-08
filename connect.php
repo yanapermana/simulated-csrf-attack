6ba463e2c8ed7ae4ad612e943d2f4d98824b48ee
yanapermana <kumpoter@gmail.com>
2017-07-08 20:14:06 +0700

    Whatever

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = new mysqli("localhost", "root", "tawheed", "exercise_csrf_attack");
+$connection = new mysqli("localhost", "root", "password", "exercise_csrf_attack");
 
 if ($connection->connect_errno) {
 	printf("Connection failed: %s\n", $connection->connect_error);
